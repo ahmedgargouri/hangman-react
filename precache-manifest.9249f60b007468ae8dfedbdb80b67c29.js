2d2996dfe17db644a9c850b8d94f8a5d326ea1a5
ahmedgargouri <gargouriahmed@outlook.fr>
2021-05-07 11:06:54 +0200

    Updates

---
+++ precache-manifest.9249f60b007468ae8dfedbdb80b67c29.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ff7cd3e15b8927d77061c4a71fcb57",
+    "url": "/hangman-react/index.html"
+  },
+  {
+    "revision": "f5cc6ae13bd4be70aa02",
+    "url": "/hangman-react/static/js/2.229f4015.chunk.js"
+  },
+  {
+    "revision": "e70dd37b120b6b0a1813",
+    "url": "/hangman-react/static/js/main.a285f5d7.chunk.js"
+  },
+  {
+    "revision": "e466c12c163347df8e5a",
+    "url": "/hangman-react/static/js/runtime~main.6eb2dcd3.js"
+  }
+]);
